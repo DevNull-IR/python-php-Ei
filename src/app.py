e4245ee024b810036269330437de8b1493c2435e
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-01-16 13:45:32 +0330

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,19 @@
 # soon Code's
+import requests
 
+def echo(value) : 
+    print(value)
 
 def print_r(**value) :
     print(value)
+
+def file_put_contents(FileName,Value = None) : 
+    if (Value == None):
+        open(FileName, "w")
+    
+    else :
+        f = open(FileName, "w")
+        f.write(Value)
+        f.close()
+    
+file_put_contents('test.py','sdsd')
